602abb4fba43431299fc2285bbeaa45617f22020
Ameen HARTLEY <ahartley@c3r2s9.wethinkcode.co.za>
2019-05-21 11:43:33 +0200

    author file and strnstr added

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 11:06:30 by ahartley          #+#    #+#             */
+/*   Updated: 2019/05/21 11:42:05 by ahartley         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strnstr(const char *whole, const char *part, size_t len)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while ((whole[i]) && (i < len))
+	{
+		while ((part[j] == whole[i]) && (i < len))
+		{
+			i++;
+			j++;
+		}
+		i = i - j;
+		if (part[j] == '\0')
+			return ((char *)&whole[i]);
+		else
+			j = 0;
+		i++;
+	}
+	return (NULL);
+}
