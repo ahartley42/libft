1b9de2c3ec0c32dda54112391e61c43efa62155b
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-23 10:46:19 +0200

    updated strnstr variables

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,29 +6,29 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:06:30 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/21 11:42:05 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/23 10:45:52 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char	*ft_strnstr(const char *whole, const char *part, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while ((whole[i]) && (i < len))
+	while ((haystack[i]) && (i < len))
 	{
-		while ((part[j] == whole[i]) && (i < len))
+		while ((needle[j] == haystack[i]) && (i < len))
 		{
 			i++;
 			j++;
 		}
 		i = i - j;
-		if (part[j] == '\0')
-			return ((char *)&whole[i]);
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
 		else
 			j = 0;
 		i++;
