4afa9c1d9ae4a7414bbe2e5c9054c57afae22e54
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-23 10:44:46 +0200

    updated strstr vars

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,29 +6,29 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:43:48 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/21 11:02:09 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/23 10:44:18 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char	*ft_strstr(const char *whole, const char *part)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while (whole[i])
+	while (haystack[i])
 	{
-		while (part[j] == whole[i])
+		while (needle[j] == haystack[i])
 		{
 			i++;
 			j++;
 		}
 		i = i - j;
-		if (part[j] == '\0')
-			return ((char *)&whole[i]);
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
 		else
 			j = 0;
 		i++;
