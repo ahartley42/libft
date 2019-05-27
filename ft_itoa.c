320b0b10fe69047a42e3a54fe62888effbdae2d7
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-27 16:35:32 +0200

    incomplete itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,32 +6,52 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:06:09 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/23 14:32:42 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/27 16:30:45 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-char	*ft_itoa(int n)
+unsigned int		ft_make_positive(int n)
 {
-	int		i;
-	char	c;
-	char	sign;
-	char	*ans;
-
-	i = 1;
-	c = '0';
 	if (n < 0)
-	{
-		sign = '-';
-		i++;
-	}
-	while ((n != 0) && (c >= '0') && (c <= '9'))
-	{
-		c = '0' + (n % 10);
-		n = n / 10;
-	ans = (char *)malloc(j * sizeof(char));
-	}
+		n = n * -1;
+	return (n);
 }
 
-//INCOMPLETE!!!
+int					ft_itoa_counter(int n, int i)
+{
+	while (n != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+char				*ft_itoa(int n)
+{
+	int			i;
+	int			z;
+	char		*ans;
+
+	i = 0;
+	z = n;
+	if (n <= 0)
+		i++;
+	(unsigned int)n = ft_make_positive(n);
+	i = ft_itoa_counter(n, i);
+	ans = (char *)malloc(i * sizeof(char) + 1);
+	ans[i] = '\0';
+	while (n != 0)
+	{
+		ans[i - 1] = (n % 10) + '0';
+		n = n / 10;
+		i--;
+	}
+	if (i == 1)
+		ans[0] = '-';
+	if (z == 0)
+		ans[0] = '0';
+	return (ans);
+}
