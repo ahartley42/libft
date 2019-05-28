52076398fe6ea18d04f95052ac1892739aa0a84e
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-28 15:32:16 +0200

    updated functional strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:25:27 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/24 15:41:53 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/28 15:31:46 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	int i;
 
 	i = 0;
-	while ((sizeof(src[i]) <= len) && (src[i]))
+	while ((len > 0) && (src[i]))
 	{
 		dst[i] = src[i];
 		i++;
-		len = len - sizeof(src[i]);
+		len--;
 	}
-	while ((int)len > 0)
+	while (len > 0)
 	{
 		dst[i] = '\0';
+		i++;
 		len--;
 	}
 	return (dst);
