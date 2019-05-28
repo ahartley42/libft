ba55d89bc9c0159dd1bf34f5d9ca4683d7d2264f
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-28 11:29:18 +0200

    updated header file and Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:12:01 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/24 09:00:41 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/28 11:28:43 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
-int					*ft_memcmp(const void *s1, const void *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memalloc(size_t size);
 void				ft_memdel(void **ap);
 char				*ft_strnew(size_t size);
