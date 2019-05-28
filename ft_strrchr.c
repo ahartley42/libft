28cb44c0920b79ee29348b28a5c8d23dfdfbb861
ahartley <ahartley@student.wethinkcode.co.za>
2019-05-28 13:45:38 +0200

    updated strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,28 +6,18 @@
 /*   By: ahartley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:13:21 by ahartley          #+#    #+#             */
-/*   Updated: 2019/05/21 14:04:26 by ahartley         ###   ########.fr       */
+/*   Updated: 2019/05/28 13:44:44 by ahartley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-size_t	ft_strlen(const char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i * sizeof(char));
-}
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int i;
 
-	i = ft_strlen(s);
-	while (i > 0)
+	i = (int)ft_strlen(s);
+	while (i >= 0)
 	{
 		if (s[i] == c)
 			return ((char *)&s[i]);
